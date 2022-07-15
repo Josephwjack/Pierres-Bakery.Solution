3558eda76b7bb2f5ac39ddb7cc23a786d23fcd2b
Josephwjack <joseph.w.jack@gmail.com>
2022-07-15 14:41:28 -0700

    add UI logic for pastry and bread orders, WIP checkout function or list

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -15,20 +15,28 @@ namespace Bakery
    }
    public static void StartBakeryMenu()
    {
-    Console.WriteLine("Welcome to Pierres Bakery")
-    Console.WriteLine("We currently have a buy 2 get 1 free on our loaves of bread. Individually priced at $" + Bread.Price " each");
-    Console.WriteLine("Pastries are $2 each. Or bundle and save 3/$5, 4/$7, 5/$9, 6/$10.")
+    Console.WriteLine("Welcome to Pierres Bakery");
+    Console.WriteLine("We currently have a buy 2 get 1 free on our loaves of bread. Individually priced at $" + Bread.Price +" each");
+    Console.WriteLine("Pastries are $2 each. Or bundle and save 3/$5, 4/$7, 5/$9, 6/$10.");
     Console.WriteLine("Please select category:");
     Console.WriteLine(" '1' for Bread |  '2' for Pastries");
     string menuType = Console.ReadLine();
     if ( menuType == "1")
     {
       Console.WriteLine("Perfect, how many loaves would you like today?");
-      int 
+      int breadAmount = int.Parse(Console.ReadLine());
+      Bread breadOrder = new Bread(breadAmount);
+      Console.WriteLine("For " + breadOrder.Quantity + " loaves of bread your total is $" + breadOrder.BreadCost());
+
+
     }
     else if( menuType == "2")
     {
-      Pastry.PastryCost();
+      Console.WriteLine("Great choice! How many pastries would you like?");
+      int pastryAmount = int.Parse(Console.ReadLine());
+      Pastry pastryOrder = new Pastry(pastryAmount);
+      Console.WriteLine("For " + pastryOrder.Quantity + " pastries your total will be $" + pastryOrder.PastryCost());
+    
     }
     else
     {
